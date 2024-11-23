d92093add9582507aba39a3a4017068e4a225419
Yi-Jacob <jacobyideveloper@gmail.com>
2024-11-22 19:14:35 -0500

    completed assessement

--- exercise.rb
+++ exercise.rb
@@ -1,18 +1,32 @@
 class Exercise
-
   # Assume that "str" is a sequence of words separated by spaces.
   # Return a string in which every word in "str" that exceeds 4 characters is replaced with "marklar".
   # If the word being replaced has a capital first letter, it should instead be replaced with "Marklar".
+  # Punctuation at the end of words should be retained.
   def self.marklar(str)
-    # TODO: Implement this method
+    str.split.map do |word|
+      # Match the word and optional punctuation
+      match = word.match(/^(\w+)(\W*)$/)
+      if match && match[1].length > 4
+        replacement = match[1][0] =~ /[A-Z]/ ? "Marklar" : "marklar"
+        replacement + match[2]
+      else
+        word
+      end
+    end.join(" ")
   end
 
   # Return the sum of all even numbers in the Fibonacci sequence, up to
   # the "nth" term in the sequence
-  # eg. the Fibonacci sequence up to 6 terms is (1, 1, 2, 3, 5, 8),
-  # and the sum of its even numbers is (2 + 8) = 10
   def self.even_fibonacci(nth)
-    # TODO: Implement this method
+    a, b = 1, 1
+    sum = 0
+  
+    (1..nth).each do
+      sum += a if a.even?
+      a, b = b, a + b
+    end
+  
+    sum
   end
-
 end
